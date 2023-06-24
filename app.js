d96879c4dbdc5c13cdad3b316639486852a3cf8a
Vedant Akare <vedantakare4@gmail.com>
2023-06-25 00:26:27 +0530

    Add existing project files to Git

--- app.js
+++ app.js
@@ -70,7 +70,7 @@ app.post("/failure", function(req,res){
 
 
 app.listen(process.env.PORT || 3000, function(){
-    console.log("Server Started on port 3000")
+    console.log("Server Started on port 3000");
     console.log(process.env.key)
 });
 
